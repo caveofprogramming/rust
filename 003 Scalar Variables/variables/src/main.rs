cb7b77d2bb49125942d4495ac39e2249ed088cd4
John <john@Johns-iMac.station>
2024-11-20 17:52:43 +0100

    Updated

---
+++ 003 Scalar Variables/variables/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    const PI:f64 = 3.141592;
+
+    println!("PI is {PI}");
+
+    let x = 10;
+    let x = 9;
+    println!("x is {x}");
+
+    let mut y = 20;
+    y = 15;
+    println!("y is {y}");
+
+    let value1:i8 = -12;
+    println!("value1 is {value1}");
+
+    // Prefixed with underscore because unused.
+    let _value2:i16 = 12;
+    let _value3:i32 = 12;
+    let _value4:i64 = 12;
+    let _value5:i128 = 12;
+    let _value6:bool = true;
+    let _value7:char = 'c';
+    let _value7:isize = 54;
+
+    let _value8:f32 = 54.0;
+    let _value9:f64 = 54.0;
+
+    let _value2:u16 = 12;
+    let _value7:usize = 54;
+
+
+}
