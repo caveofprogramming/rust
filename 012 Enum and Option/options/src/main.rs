4e5e56b58c59e49f582dccc21f5f4bd7ded03999
John <john@Johns-iMac.station>
2024-12-19 17:06:18 +0100

    Updated

---
+++ 012 Enum and Option/options/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    #[derive(Debug)]
+    enum StarType {
+        RedDwarf = 0,
+        YellowDwarf = 7,
+        RedGiant = 10,
+    }
+
+    let star: StarType = StarType::RedGiant;
+    println!("{star:?}");
+
+    #[derive(Debug)]
+    enum Entity {
+        Star(String, StarType),
+        Planet(String),
+    }
+
+    let sun: Entity = Entity::Star(String::from("The Sun"), StarType::YellowDwarf);
+    println!("{sun:?}");
+
+    let value = Some(99);
+    println!("{value:?}");
+    
+    let mut value:Option<String> = None;
+    value = Some(String::from("Greeting"));
+    println!("{value:?}");
+}
