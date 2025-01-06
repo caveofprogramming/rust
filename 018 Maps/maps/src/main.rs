00695155bd2d702696c1e8fca9fb4615d02c19ee
John <john@Johns-iMac.station>
2025-01-06 22:33:29 +0100

    Updated.

---
+++ 018 Maps/maps/src/main.rs
@@ -0,0 +1,44 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut people = HashMap::new();
+
+    people.insert("Bob", 24);
+    people.insert("Sue", 36);
+    people.insert("Rita", 72);
+
+    for (name, age) in &people {
+        println!("{name}: {age}");
+    }
+
+    let bob_age = people.get("Bob");
+
+    println!("{}", bob_age.copied().unwrap_or(0));
+
+    if let Some(value) = people.get("Sue") {
+        println!("Sue is: {value}");
+    };
+
+    let key = "Some key".to_string();
+    let value = "Some value".to_string();
+
+    let mut test_map = HashMap::new();
+    test_map.insert(key, value);
+
+    //println!("{key}");
+    //println!("{value}");
+
+    people.insert("Bob", 48);
+    println!("{people:?}");
+
+    people.entry("Bob").or_insert(86);
+    people.entry("Ethel").or_insert(22);
+    println!("{people:?}");
+
+    people.entry("Bob").and_modify(|age| *age += 100);
+    println!("{people:?}");
+    
+    let age_ethel = people.entry("Ethel").or_insert(0);
+    *age_ethel += 20;
+    println!("{people:?}");
+}
