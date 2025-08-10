a68f3106844c9aab54f599bf80f814756aa95872
Cave of Programming <caveofprogramming@Johns-MacBook-Air.local>
2025-08-10 17:02:56 +0200

    Updated

--- 036 Mutexes/mutexes/src/main.rs
+++ 036 Mutexes/mutexes/src/main.rs
@@ -1,3 +1,26 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let m = Arc::new(Mutex::new(0));
+
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let m = m.clone();
+
+        let handle = thread::spawn(move || {
+            let mut counter = m.lock().unwrap();
+
+            *counter += 1;
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("value: {:?}", *m.lock().unwrap());
 }
